89fde574690b7e442ab74b7f355845fb56ddc9cc
James <jameseh4@hawaii.edu>
2025-05-22 22:09:19 -0400

    Minimum working version of fieldlines.c

--- DataGeneration/FIMS_Geometry/fieldlines.C
+++ DataGeneration/FIMS_Geometry/fieldlines.C
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
 #include <cmath>
 #include <TCanvas.h>
 #include <TApplication.h>
@@ -41,42 +38,14 @@ std::cout << "----------------------------------------------------------------\n
   double x0, y0 ,z0 ,x1 ,y1 ,z1;
   elm->GetBoundingBox(x0, y0 ,z0 ,x1 ,y1 ,z1);
   //constexpr double pitch = .001249;
-  double pitch  = double(x1)-.00001;
+  double pitch  = double(x1)-.0001;
   const double xmin = -1*pitch;
   const double xmax =  1*pitch;
   const double ymin = -1*pitch;
   const double ymax =  1*pitch;
   const double zmin = double(z0)+.00001;
   const double zmax =  double(z1)-.00001;
-  
-  
-  std::ifstream paramFile;
-  paramFile.open("input_file/runControl.txt");
-  std::string curLine;
-  std::map<std::string, std::string> readParam;
-  //Read the file contents to a map
-  int numKeys = 0;
-  while(std::getline(paramFile, curLine)){
-  //if statement ignores the commented lines in runControl.txt
-    if(curLine.find('/') == 0){
-      continue;
-    }
-
-    size_t keyPos = curLine.find("=");
-    if (keyPos != std::string::npos){
-      std::string key = curLine.substr(0, keyPos - 1);
-      std::string value = curLine.substr(keyPos + 2);
-      if(value.back() == ';'){
-        value.pop_back();
-      }
-
-      readParam[key] = value;
-      numKeys++;
-    }
-  }
-  paramFile.close();
-  int numFieldLine;
-  numFieldLine = std::stoi(readParam["numFieldLine"]);
+  int num = 100;
   
   std::cout << "----------------------------------------------------------------\n";
   // Get the extent of the field map
@@ -87,6 +56,7 @@ std::cout << "----------------------------------------------------------------\n
   std::cout << "----------------------------------------------------------------\n";
   elm->PrintMaterials();
   std::cout << "----------------------------------------------------------------\n";
+  
  //setup the sensor
  Sensor sensor;
   sensor.AddComponent(elm);
@@ -95,34 +65,30 @@ std::cout << "----------------------------------------------------------------\n
   
 //create a vector of arrays that can store the data points along each field line
   DriftLineRKF drift(&sensor);
-  std::cout << "RKF initialized\n";
-  drift.SetMaximumStepSize(.0001);
-  std::cout << "step size set\n";
+  drift.SetMaximumStepSize((zmax-zmin)/1000);
   std::vector<std::array<float, 3> > Line;
-  std::cout << "vector created\n";
   std::ofstream driftline;
-  std::cout << "driftlines made\n";
-  driftline.open("output_file/driftline_Diag.csv");
+  std::cout << "driftlines initialized\n";
   
-  std::cout << "The number of Fieldlines is:" << readParam["numFieldLine"] << "\n";
-  std::cout << "----------------Printing Fieldlines--------------------------\n";
+  driftline.open("output_file/driftlineDiag.csv");
   
 //loop for finding all of the field points and storing them on a CSV
-  int driftProgress = -1;
-  for(int count=0; count<=numFieldLine; ++count)
+  for(int count=1; count<=num; ++count)
       {
-      drift.FieldLine(xmin*(count/numFieldLine), ymin*(count/numFieldLine), zmax, Line);
+      drift.FieldLine(xmin+2*count*xmax/num, ymin+2*count*ymax/num, zmax, Line);
       auto size = Line.size();
+      //std::cout << "The size of the list is:" << size << "\n";
+      
       for(int i = 0; i<size; ++i)
         {
-        //std::cout << "x = " << Line[i][0] << ", y = " << Line[i][1] << ","<< ",  z = " << Line[i][2] << "\n";
         driftline << Line[i][0] << "," << Line[i][1] << "," << Line[i][2] << "\n";
         }
-        ++driftProgress;
-        int remainder = driftProgress % 10;
-        if (remainder == 0){
-          std::cout << "Current Driftline Progress:" << driftProgress << "%\n";
+      int driftprogress = (count*100/num);
+      
+      if(driftprogress % 10 == 0){
+          std::cout << "Driftline Progress: " << static_cast<float>(count)*100/num << "%" << "\n";
           }
+
       }
    driftline.close();
   
