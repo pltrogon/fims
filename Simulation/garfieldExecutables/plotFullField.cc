34a7288434607fc11a75813398ea82abaf256e5f
James <jameseh4@hawaii.edu>
2026-05-29 14:55:27 -1000

    Fixed input number check in plotFullField

--- Simulation/garfieldExecutables/plotFullField.cc
+++ Simulation/garfieldExecutables/plotFullField.cc
@@ -35,7 +35,7 @@
 using namespace Garfield;
 
 int main(int argc, char * argv[]) {
-  if(argc != 1){
+  if(argc != 2){
       std::cerr << "Format: " << argv[0] << " <GeometryMode>" << std::endl;
       return -1;
   }
